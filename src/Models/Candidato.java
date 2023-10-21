61f5a554864f28c560af080447aec8ecb255e85d
Tucozz <tutuestefanato@gmail.com>
2023-10-21 17:55:04 -0300

    candidato now superclass

--- src/Models/Candidato.java
+++ src/Models/Candidato.java
@@ -8,4 +8,14 @@ public class Candidato {
     private LocalDate dataNascimento;
     private boolean sexo; // internamente o sexo sim é masculino e não é feminino
     private Cargo cargo;
+
+    public Candidato(String numero, Partido partido, String nome, String nomeUrna, LocalDate dataNascimento, boolean sexo, Cargo cargo) {
+        this.numero = numero;
+        this.partido = partido;
+        this.nome = nome;
+        this.nomeUrna = nomeUrna;
+        this.dataNascimento = dataNascimento;
+        this.sexo = sexo;
+        this.cargo = cargo;
+    }
 }
--- src/Models/CandidatoVotacao.java
+++ src/Models/CandidatoVotacao.java
@@ -1,6 +1,12 @@
-public class CandidatoVotacao {
-    private Candidato candidato;
+import java.time.LocalDate;
+
+public class CandidatoVotacao extends Candidato{
     private boolean eleito;
     private int numeroVotos;
     
+    public CandidatoVotacao(String numero, Partido partido, String nome, String nomeUrna, LocalDate dataNascimento, boolean sexo, Cargo cargo, boolean eleito, int numeroVotos) {
+        super(numero, partido, nome, nomeUrna, dataNascimento, sexo, cargo);
+        this.eleito = eleito;
+        this.numeroVotos = numeroVotos;
+    }
 }
