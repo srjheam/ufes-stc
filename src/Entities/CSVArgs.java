1dd99c786c8f921d435ed0470a231bd923fab613
Tucozz <tutuestefanato@gmail.com>
2023-10-21 23:34:50 -0300

    Full CSV Readers

--- src/Entities/CSVArgs.java
+++ src/Entities/CSVArgs.java
@@ -4,6 +4,16 @@ import java.util.List;
 public class CSVArgs {
     private LinkedList<String> importantColumns;
 
+    public CSVArgs(){
+        importantColumns = new LinkedList<String>();
+    }
+    
+    public CSVArgs(String[] args){
+        for(String arg : args){
+            importantColumns.add(arg);
+        }
+    }
+
     public void addImportantColumn(String column){
         importantColumns.add(column);
     }
--- src/Utils/Reader/CSVReader.java
+++ src/Utils/Reader/CSVReader.java
@@ -1,12 +1,15 @@
-package Utils.Reader;
-
 import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Scanner;
 
 public class CSVReader {
     
-    public static void readCandidatosCSV(String path, CSVArgs args){
+    public static String[][] genericCSVread(String path, CSVArgs args){
         try(FileInputStream fin = new FileInputStream(path); 
         Scanner s = new Scanner(fin, "ISO-8859-1")){
             LinkedList<String> columns = new LinkedList<String>();
@@ -18,25 +21,74 @@ public class CSVReader {
                     columns.add(token);
                 }
             }
+            String[][] data = new String[(int)quantasLinhas(path)][args.getImportantColumns().size()];
+            int j = 0;
             while(s.hasNextLine()){
-                int i = 0, j = 0;
-                String[] candidatoArgs = new String[args.getImportantColumns().size()];
+                String[] importantArgs = new String[args.getImportantColumns().size()];
                 String line = s.nextLine();
                 try(Scanner lineScanner = new Scanner(line)){
+                    int i = 0;
                     lineScanner.useDelimiter(";");
                     for(String arg : columns){
                         if(args.getImportantColumns().contains(arg)){
-                            candidatoArgs[i++] = lineScanner.next();
+                            importantArgs[i++] = lineScanner.next();
                         }
                         else{
                             lineScanner.next();
                         }
                     }
                 }
-                CandidatoVotacao candidato = parseCandidato(candidatoArgs);
+                data[j++] = importantArgs;
             }
+
+            return data;
+        
         }catch(Exception e){
             e.printStackTrace();
         }
     }
+
+    public static List<CandidatoVotacao> readCandidatosCSV(String path){
+        String[] importantValues = {"CD_CARGO", "CD_SITUACAO_CANDIDATO_TOT", "NR_CANDIDATO", "NM_URNA_CANDIDATO",
+                                    "NR_PARTIDO", "SG_PARTIDO", "NR_FEDERACAO", "DT_NASCIMENTO", "CD_SIR_TOT_TURNO",
+                                    "CD_GENERO", "NM_TIPO_DESTINACAO_VOTOS", "CD_SITUACAO_CANDIDATO_TOT"};
+        CSVArgs args = new CSVArgs(importantValues);
+
+        String[][] data = genericCSVread(path, args);
+
+        List<CandidatoVotacao> candidatos = new LinkedList<CandidatoVotacao>();
+        for(String[] candidato : data){
+            candidatos.add(CSVParser.parseCandidato(candidato));
+        }
+
+        return candidatos;
+    }
+
+    public static List<LegendaVotacao> readPartidosCSV(String path){
+        String[] importantValues = {"CD_CARGO", "NR_VOTAVEL", "QT_VOTOS"};
+        CSVArgs args = new CSVArgs(importantValues);
+
+        String[][] data = genericCSVread(path, args);
+
+        List<LegendaVotacao> votacao = new LinkedList<LegendaVotacao>();
+        for(String[] legenda : data){
+            votacao.add(CSVParser.parseVotacao(legenda));
+        }
+
+        return votacao;
+    }
+
+    public static int quantasLinhas(String fileName) {
+      Path path = Paths.get(fileName);
+
+      int lines = 0;
+      try {
+          lines = (int)Files.lines(path).count();
+      } catch (IOException e) {
+          e.printStackTrace();
+      }
+
+      return lines;
+
+  }
 }
